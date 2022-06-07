f763eb8828afa785f5f1c1865e6c892397a36fdb
websnapshot <websnapshot>
2022-06-07 16:51:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# websnapshot
+
+[![CC BY-NC-SA 4.0][cc-by-nc-sa-shield]][cc-by-nc-sa] ![project status][status-shield]
+
+This work is licensed under a
+[Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License][cc-by-nc-sa].
+
+[cc-by-nc-sa]: LICENSE
+[cc-by-nc-sa-shield]: https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-informational.svg
+[status-shield]: https://img.shields.io/badge/status-maintenance%20mode-orange
---
+++ src/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using toolbelt;
+using webgrep;
+
+namespace websnapshot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Error.WriteLine("websnapshot");
+            using (FirefoxInstance browser = new FirefoxInstance())
+            {
+                Console.Error.WriteLine($"navigate to {args[0]}");
+                if (browser.NavigateTo(args[0]))
+                {
+                    string title = browser.AttachedElements("title").First().GetText();
+                    Console.Error.WriteLine($"title is {title}");
+                    foreach (char elem in Path.GetInvalidFileNameChars().Union(Path.GetInvalidPathChars()))
+                    {
+                        title = title.Replace(elem, '_');
+                    }
+                    string filename = $"{title}.jpg";
+                    Console.Error.WriteLine($"store snapshot in {filename}");
+                    using (MemoryStream mem = new MemoryStream())
+                    {
+                        if (browser.TakeScreenshot(mem))
+                        {
+                            mem.Seek(0, SeekOrigin.Begin);
+                            using (var image = new Bitmap(System.Drawing.Image.FromStream(mem)))
+                            {
+                                image.Save(filename, ImageFormat.Jpeg);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
